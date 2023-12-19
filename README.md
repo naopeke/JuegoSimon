e092b84a2ff716e8468ac908e437ccdc832b9655
naopeke <naopeke@protonmail.com>
2023-12-19 10:26:54 +0100

    firt commit

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# JuegoSimon
+# Simon Game  
+Description | Technologies | Installation | How to play | Author  
+  
+# Description  
+Crea un juego Simón con las siguientes características:  
+  
+1. Debe contener un botón de iniciar el juego, un botón para procesar la respuesta  del código de colores, un botón para continuar si se introdujo correctamente la combinación y por último un botón para resetear el juego.  
+  
+2. Al pulsar el botón de iniciar el juego nos muestra una combinación de colores a repetir.  
+  
+3. Al pulsar sobre el botón de procesar se compara la combinación de colores introducida por el mostrada con la introducida por el usuario.  
+  
+4. Informamos al usuario si ha introducido la combinación de colores correctamente.  
+Si el resultado es correcto el botón continuar eleva la dificultad del juego (combinación más larga) y comienza la siguiente ronda , si no se repite el nivel.  
+  
+5. El botón para resetear reinicia el juego sin refrescar la página.  
+  
+# Technologies  
+HTML, CSS, JavaScript  
+
+# Installation  
+Clone Repository  
+```
+git clone https://github.com/naopeke/Simon-Game.git
+```
---
+++ script.js
@@ -0,0 +1,146 @@
+class SimonGame {
+    constructor() {
+        this.level = 0;
+        this.sequence = [];
+        this.humanSequence = [];
+        this.boxes = document.getElementsByClassName('colorbox');
+        this.levelElement = document.getElementById('level');
+    }
+
+    //Make a next level
+    nextLevel() {
+        this.level++;
+        this.sequence.push(this.randomBox());
+        this.showSequence();
+        this.showLevel();
+    }
+
+    //Make a random index of the boxes
+    randomBox() {
+        let randomIndex = Math.floor(Math.random() * this.boxes.length);
+        return this.boxes[randomIndex];
+    }
+
+    //Brighten when the user clicks
+    brightenBox(box) {
+        box.classList.add('highlight');
+
+        setTimeout(() => {
+            box.classList.remove('highlight');
+        }, 500);
+    }
+
+
+    //setTimeout は一度だけ遅延して実行される。
+    //setInterval は繰り返し定期的に実行される。
+    //Display the sequence to the user. When it finishes, clear the interval.
+    showSequence() {
+        let index = 0;
+        let settingInterval = setInterval(() => {
+            this.brightenBox(this.sequence[index]);
+            index++;
+            if (index === this.sequence.length) {
+                clearInterval(settingInterval);
+            }
+        }, 1000);
+    }
+
+    //ユーザーがクリックしたボックス（box）を humanSequence 配列の末尾に追加
+    //Add the array of the clicking history into the array of humanSequence
+    addHumanSequence(box) {
+        this.humanSequence.push(box)
+    }
+
+    //Check if the answer is right
+    checkSequence() {
+        for (let i = 0; i < this.humanSequence.length; i++) {
+            if (this.sequence[i] !== this.humanSequence[i]) {
+                document.getElementById('commentbox').innerText = 'WRONG. Try again!';
+                this.humanSequence = [];
+                this.showSequence();
+                document.getElementById('continue').style.display = 'none';
+                return;
+            }
+        }
+
+        if (this.sequence.length === this.humanSequence.length) {
+            document.getElementById('commentbox').innerText = 'CORRECT! Continue to the next level!';
+            this.humanSequence = [];
+            // this.nextLevel();
+            setTimeout (()=> {
+                // this.nextLevel();
+                document.getElementById('continue').style.display = 'block';
+            }, 500);
+
+        }
+    }
+
+    showLevel(){
+        this.levelElement.innerText = `Level: ${this.level}`;
+    }
+
+    //Reset the game. Clear the level, the sequence and the humanSequence
+    resetGame() {
+        this.level = 1;
+        this.sequence = [];
+        this.humanSequence = [];
+    }
+
+
+
+}
+
+
+let simonGame;
+
+document.getElementById('start').addEventListener('click', () => {
+    simonGame = new SimonGame();
+    simonGame.nextLevel();
+
+    for (let box of simonGame.boxes) {
+        box.addEventListener('click', () => {
+            simonGame.addHumanSequence(box);
+        });
+    }
+});
+
+document.getElementById('process').addEventListener('click', () => {
+    simonGame.checkSequence();
+});
+
+document.getElementById('continue').addEventListener('click', () => {
+    simonGame.nextLevel();
+    document.getElementById('continue').style.display = 'none';
+    setTimeout(() => {
+       document.getElementById('commentbox').innerText = ''; 
+    }, 300);
+});
+
+document.getElementById('reset').addEventListener('click', () => {
+    simonGame.resetGame();
+});
+
+
+
+//To Make a random color directly with rgb
+// const button = document.querySelector('button');
+// let h1 = document.querySelector('h1');
+
+// button.addEventListener('click', () => {
+// const newColor = makeRandColor();
+
+// document.body.style.backgroundColor = newColor;
+// h1.innerText = newColor;
+
+// });
+
+// const makeRandColor = () => {
+
+//     const r = Math.floor(Math.random() * 256);
+//     const g = Math.floor(Math.random() * 256);
+//     const b = Math.floor(Math.random() * 256);
+
+//     return `rgb(${r}, ${g}, ${b}`
+// }
+
+
